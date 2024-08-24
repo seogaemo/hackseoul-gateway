3c7fe9464d9d9ff31407d6e52c6d1b82741d4762
suk-6 <me@suk.kr>
2024-08-25 08:55:46 +0900

    feat: Add delegate permissions for web and android_app

--- src/modules/well-known/well-known.controller.ts
+++ src/modules/well-known/well-known.controller.ts
@@ -15,6 +15,23 @@ export class WellKnownController {
           ],
         },
       },
+      {
+        relation: ["delegate_permission/common.get_login_creds"],
+        target: {
+          namespace: "web",
+          site: "https://hackseoul.plebea.com",
+        },
+      },
+      {
+        relation: ["delegate_permission/common.get_login_creds"],
+        target: {
+          namespace: "android_app",
+          package_name: "com.seogaemo.hackseoul_android",
+          sha256_cert_fingerprints: [
+            "3D:41:D2:E9:0C:01:88:96:AE:06:50:A2:BF:4B:38:9B:7B:E2:5A:9E:FA:02:20:D2:7E:AE:E0:F4:72:D7:F8:E7",
+          ],
+        },
+      },
     ];
   }
 }
