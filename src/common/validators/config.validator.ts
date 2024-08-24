94d8d68cbfd036a7dfa552a795ac20f7e282c58f
suk-6 <me@suk.kr>
2024-08-25 08:05:23 +0900

    chore: Update default port in ConfigValidator

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const ConfigValidator = Joi.object({
-  PORT: Joi.number().default(3000),
+  PORT: Joi.number().default(80),
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
     .default("development"),
