10c73e197da4fddd55b9eddbf33b0bbb9e2cb6df
suk-6 <me@suk.kr>
2024-08-25 00:59:14 +0900

    feat: Update auth module to use gRPC client for authentication

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,21 @@
+import { AUTH_PACKAGE_NAME } from "@shared/generated/auth.proto";
+import { grpcClientOptions } from "@shared/options/auth.option";
+
 import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 import { AuthController } from "./auth.controller";
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: grpcClientOptions.options,
+      },
+    ]),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
