bca2b072a954292494f200f6c54ccbd8b943092d
suk-6 <me@suk.kr>
2024-08-25 08:05:34 +0900

    Revert "chore: Update default port in ConfigValidator"
    This reverts commit 94d8d68cbfd036a7dfa552a795ac20f7e282c58f.

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const ConfigValidator = Joi.object({
-  PORT: Joi.number().default(80),
+  PORT: Joi.number().default(3000),
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
     .default("development"),
