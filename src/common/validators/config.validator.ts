5245df8ce562c07e2108eb4d4e0fbd2a79e7c626
suk-6 <me@suk.kr>
2024-08-25 01:34:14 +0900

    chore: Update default NODE_ENV value to "development" in config validator

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -4,7 +4,7 @@ export const ConfigValidator = Joi.object({
   PORT: Joi.number().default(3000),
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
-    .default("production"),
+    .default("development"),
   // DATABASE
   DATABASE_URL: Joi.string().required(),
   // CORS
