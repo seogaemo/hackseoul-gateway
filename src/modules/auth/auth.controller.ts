eb1e648e7462f8e47f9f8d1f291fa4ae50665235
suk-6 <me@suk.kr>
2024-08-25 01:34:18 +0900

    feat: Add gRPC client for authentication in auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from "@nestjs/common";
+import {
+  AUTH_PACKAGE_NAME,
+  AuthServiceClient,
+} from "@shared/generated/auth.proto";
+import { AUTH_SERVICE_NAME } from "@shared/generated/auth.proto";
+import { firstValueFrom } from "rxjs";
+
+import {
+  Body,
+  Controller,
+  Inject,
+  OnModuleInit,
+  Post,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { ClientGrpc } from "@nestjs/microservices";
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
+
+import { SignInUserDTO, SignInUserResponseDTO } from "./dto/signin-user.dto";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController implements OnModuleInit {
+  constructor(
+    @Inject(AUTH_PACKAGE_NAME)
+    private readonly zoneClient: ClientGrpc,
+  ) {}
+
+  private authService!: AuthServiceClient;
+
+  onModuleInit() {
+    this.authService =
+      this.zoneClient.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  @Post("signin")
+  @ApiBody({
+    type: SignInUserDTO,
+  })
+  @ApiOperation({ summary: "User Login" })
+  @ApiOkResponse({
+    description: "User Login Success",
+    type: SignInUserResponseDTO,
+  })
+  @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  async signIn(@Body() dto: SignInUserDTO) {
+    try {
+      return firstValueFrom(this.authService.signIn(dto));
+    } catch (e) {
+      throw new UnauthorizedException("Unauthorized");
+    }
+  }
+}
