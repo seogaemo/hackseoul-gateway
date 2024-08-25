d0262f5e933aadfc13636a39d11669e2055e5e9f
suk-6 <me@suk.kr>
2024-08-25 10:36:31 +0900

    chore: CORS 해제

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 
 import { Logger, ValidationPipe } from "@nestjs/common";
-import { CustomOrigin } from "@nestjs/common/interfaces/external/cors-options.interface";
+// import { CustomOrigin } from "@nestjs/common/interfaces/external/cors-options.interface";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 
@@ -16,26 +16,26 @@ async function bootstrap() {
   const servicePort = config.get<string>("PORT", "3000");
   const NODE_ENV = config.get<string>("NODE_ENV", "development");
 
-  let CORS_ORIGIN:
-    | boolean
-    | string
-    | RegExp
-    | (string | RegExp)[]
-    | CustomOrigin = [];
-  if (config.get<string>("CORS_ORIGIN")) {
-    CORS_ORIGIN.push(...config.get<string>("CORS_ORIGIN", "*").split(","));
-  }
-  if (config.get<string>("CORS_REGEX_ORIGIN")) {
-    CORS_ORIGIN.push(
-      ...config
-        .get<string>("CORS_REGEX_ORIGIN", "")
-        .split(",")
-        .map((regex) => new RegExp(regex)),
-    );
-  }
-  if (CORS_ORIGIN.length === 0) {
-    CORS_ORIGIN = true;
-  }
+  // let CORS_ORIGIN:
+  //   | boolean
+  //   | string
+  //   | RegExp
+  //   | (string | RegExp)[]
+  //   | CustomOrigin = [];
+  // if (config.get<string>("CORS_ORIGIN")) {
+  //   CORS_ORIGIN.push(...config.get<string>("CORS_ORIGIN", "*").split(","));
+  // }
+  // if (config.get<string>("CORS_REGEX_ORIGIN")) {
+  //   CORS_ORIGIN.push(
+  //     ...config
+  //       .get<string>("CORS_REGEX_ORIGIN", "")
+  //       .split(",")
+  //       .map((regex) => new RegExp(regex)),
+  //   );
+  // }
+  // if (CORS_ORIGIN.length === 0) {
+  //   CORS_ORIGIN = true;
+  // }
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -45,12 +45,16 @@ async function bootstrap() {
   );
 
   app.use(cookieParser());
+  // app.enableCors({
+  //   origin: CORS_ORIGIN,
+  //   methods: config.get<string>("CORS_METHODS", "GET,PUT,PATCH,POST,DELETE"),
+  //   credentials: config.get<boolean>("CORS_CREDENTIALS", true),
+  //   preflightContinue: config.get<boolean>("CORS_PREFLIGHT", false),
+  //   optionsSuccessStatus: config.get<number>("CORS_OPTIONS_STATUS", 204),
+  // });
   app.enableCors({
-    origin: CORS_ORIGIN,
-    methods: config.get<string>("CORS_METHODS", "GET,PUT,PATCH,POST,DELETE"),
-    credentials: config.get<boolean>("CORS_CREDENTIALS", true),
-    preflightContinue: config.get<boolean>("CORS_PREFLIGHT", false),
-    optionsSuccessStatus: config.get<number>("CORS_OPTIONS_STATUS", 204),
+    origin: true,
+    credentials: true,
   });
 
   await SGMSwaggerSetup(app);
